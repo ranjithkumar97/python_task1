08c7c8ebf8f85dc3d2013bd7f0736146261abc5c
santhosh kumar <sandysanthosh101@gmail.com>
2019-01-28 23:38:09 +0530

    Working with tasks

---
+++ Reversion.py
@@ -0,0 +1,10 @@
+import datetime
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+print("yopur results")
+x = datetime.datetime.now()
+print(x)
